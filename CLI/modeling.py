c9c5ebc9819a28f4232c0d696c9f83e22fb1c50e
Shalini Ananda <50239203+ShaliniAnandaPhD@users.noreply.github.com>
2024-06-05 10:22:10 -0700

    Create modeling.py

---
+++ CLI/modeling.py
@@ -0,0 +1,26 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+def train_model(data):
+    """
+    Train a machine learning model to predict marine species populations.
+
+    Args:
+        data (pd.DataFrame): Preprocessed data for training.
+
+    Returns:
+        RandomForestRegressor: Trained random forest model.
+    """
+    X = data.drop(['species_population'], axis=1)
+    y = data['species_population']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    
+    y_pred = model.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f"Mean Absolute Error: {mae}")
+    
+    return model
